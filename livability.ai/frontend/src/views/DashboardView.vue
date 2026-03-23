2b59d341ab47b2add9379417f47a082c921f3e2a
Ali Al-Mahanna <alialmahanna@gmail.com>
2026-03-24 00:26:14 +0800

    began building header

---
+++ livability.ai/frontend/src/views/DashboardView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class = "container">
+  <div class="row">
+    <div class = "col-9 border border-light-subtle"> yes
+    </div>
+  </div>
+
+</div>
+</template>
+
+<style scoped>
+
+</style>
